c34f8365f2c7ee4ebf6276043d10485a6c39e0bf
Cláudio Gomes <cla.gms@gmail.com>
2016-12-29 21:57:26 +0100

    added FMI lybrary bt can't install it yet

---
+++ SemanticAdaptationForFMI/Experiments/case_study_modelica_only/get_fmus.ps1
@@ -0,0 +1,4 @@
+xcopy "$HOME\AppData\Local\Temp\OpenModelica\OMEdit\PW_PowerSystem.fmu" . /Y /D
+xcopy "$HOME\AppData\Local\Temp\OpenModelica\OMEdit\PW_Window.fmu" . /Y /D
+xcopy "$HOME\AppData\Local\Temp\OpenModelica\OMEdit\Obstacle.fmu" . /Y /D
+pause
