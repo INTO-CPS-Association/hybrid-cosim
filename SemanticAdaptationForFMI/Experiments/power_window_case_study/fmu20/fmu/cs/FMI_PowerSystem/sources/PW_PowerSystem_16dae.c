04945806f451234e54cdd3124c9324743faf2872
Cláudio Gomes <cla.gms@gmail.com>
2017-04-06 16:18:43 +0200

    cleanup

--- SemanticAdaptationForFMI/Experiments/power_window_case_study/fmu20/fmu/cs/FMI_PowerSystem/sources/PW_PowerSystem_16dae.c
+++
@@ -1,9 +0,0 @@
-/* DAE residuals is empty */
- #include "PW_PowerSystem_model.h"
-#ifdef __cplusplus
-extern "C" {
-#endif
-int PW_PowerSystem_initializeDAEmodeData(DATA *inData, DAEMODE_DATA* daeModeData){ return -1; }
-#ifdef __cplusplus
-}
-#endif
